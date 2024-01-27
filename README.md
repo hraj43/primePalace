a90354d602f8d4da92e2b51a79c5f3ff4e235c4a
harshraj438524 <21bec050@iiitdmj.ac.in>
2024-01-27 17:21:34 +0530

    first-commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# free4talk - Chat Application 
+
+
+![login page](./images/free4talk_login.png)
+
+![home page](./images/free4talk.png)
+
+## Installation Guide
+
+### Requirements
+- [Nodejs](https://nodejs.org/en/download)
+- [Mongodb](https://www.mongodb.com/docs/manual/administration/install-community/)
+
+Both should be installed and make sure mongodb is running.
+
+
+Now rename env files from .env.example to .env
+```shell
+cd public
+mv .env.example .env
+cd ..
+cd server
+mv .env.example .env
+cd ..
+```
+
+Now install the dependencies
+```shell
+cd server
+yarn
+cd ..
+cd public
+yarn
+```
+We are almost done, Now just start the development server.
+
+For Frontend.
+```shell
+cd public
+yarn start
+```
+For Backend.
+
+Open another terminal in folder, Also make sure mongodb is running in background.
+```shell
+cd server
+yarn start
+```
+
+Done! Now open localhost:3000 in your browser.
---
+++ client/src/firebase.js
@@ -0,0 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:"c3aaaf0672c5493f10e4c8fe8bb3be09c15e518a",
+  authDomain: 'mern-estate.firebaseapp.com',
+  projectId: 'mern-estate',
+  storageBucket: 'mern-estate.appspot.com',
+  messagingSenderId: '1078482850952',
+  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// import { initializeApp } from "firebase/app";
+
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAyBlZGcRA5sCY66G1SAgN2Nj8YlJj_oXQ",
+//   authDomain: "realestate-20669.firebaseapp.com",
+//   projectId: "realestate-20669",
+//   storageBucket: "realestate-20669.appspot.com",
+//   messagingSenderId: "442658156012",
+//   appId: "1:442658156012:web:d8812dc1bf2f3dd22ce4c9",
+//   measurementId: "G-SJH2GQKX4C"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
